f27ee20de1b5f0f27d4c0559edadabe36b86fb9e
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-03-17 21:06:11 +0700

    test

---
+++ KienThucTrinhDuyetVaAPI/app.js
@@ -0,0 +1,22 @@
+const xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    const res = JSON.parse(this.responseText);
+    let html = "";
+
+    res.data.forEach((item) => {
+      html += `
+      
+            <div>${item.email}</div> 
+      
+            <img src=${item.avatar} alt="" />
+        `;
+    });
+    document.getElementById("demo").innerHTML = html;
+    console.log(res);
+  }
+};
+xhttp.open("GET", "https://reqres.in/api/users", true);
+
+xhttp.send();
---
+++ Storage/app.js
@@ -0,0 +1,62 @@
+// let check = 10;
+
+// const handle = () => {
+//   return [123].map((item) => item * 2);
+// };
+
+// let user = "d";
+
+// let value = user ?? "Hello";
+
+// console.log(value);
+
+// function Cat(name, color, type) {
+//   this.name = name;
+//   this.color = color;
+//   this.type = type;
+
+//   this.meox = function () {
+//     console.log(`${this.name} meox: meo meo meo`);
+//   };
+// }
+
+// let alex = new Cat("Khoa", "yellow", " bengal");
+
+// console.log(alex.meox());
+
+// const num = [1, 2, 3];
+
+// const callback = (item, index) => {
+//   console.log(`STT ${index} là ${item}`);
+// };
+
+// num.forEach(callback);
+
+// const num = [1, 2, 3, 4, 5, 6];
+
+// const pFunc = () =>
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       reject("Hello success");
+//     }, 1000);
+//   });
+
+// const value = pFunc()
+//   .then((value) => {
+//     console.log(value);
+//     return 100;
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     return -100;
+//   })
+//   .finally(() => {
+//     console.log("Ket thuc");
+//   });
+// value.then((value) => {
+//   console.log(value);
+//});
+
+// assyn await
+
+//2023 - 03 - 12 6: 00: 00
