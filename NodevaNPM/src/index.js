bafc9c08c72c4900afa263708df8bc7a3b71d7b7
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-03-22 11:41:56 +0700

    html webpack plugin, mini css plugin

---
+++ NodevaNPM/src/index.js
@@ -0,0 +1,4 @@
+import sum from "./utils";
+import "./styles/style.css";
+import "./styles/style.scss";
+console.log(sum(100, 30));
---
+++ NodevaNPM/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    app: path.resolve("src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss|css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
