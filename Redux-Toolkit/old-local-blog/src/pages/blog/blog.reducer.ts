89f09e86b4384d893bd61ecc9af81878b8467a1f
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-08-01 16:34:23 +0700

    Handle addPost trong reducer với builder callback

--- Redux-Toolkit/old-local-blog/src/pages/blog/blog.reducer.ts
+++ Redux-Toolkit/old-local-blog/src/pages/blog/blog.reducer.ts
@@ -13,6 +13,15 @@ const initialState: BlogState = {
 // tạo action, truyền type là Post
 export const addPost = createAction<Post>('blog/addPost')
 // builderCallback là nơi xử lí action và update state trong này
-const blogReducer = createReducer(initialState, (builder) => {})
+const blogReducer = createReducer(initialState, (builder) => {
+  // addCase nhận 2 tham số: 1.Là addPost, 2. là callback
+  // builder nhu là func reducer
+  // immerjs giúp cta mutate state an toàn
+  builder.addCase(addPost, (state, action) => {
+    // push action.payload
+    const post = action.payload
+    state.postList.push(post)
+  })
+})
 
 export default blogReducer
